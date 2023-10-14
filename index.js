3103c29f52fb39e9603eb7fc6b7ce0ddf4bede59
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-14 13:51:45 +0300

    Express server & ENV variables

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
