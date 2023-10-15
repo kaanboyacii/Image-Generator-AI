c64e6ec15403c4e85fe8e9083345946c060e9f0c
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-15 20:41:19 +0300

    openai Routes

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use('/openai',require('./routes/openaiRoutes'))
+
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
